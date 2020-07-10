d33285814b0b8a0ab391b8d66e775e8315efd199
unknown <snehanimbare15@gmail.com>
2020-07-10 16:29:06 +0530

    Store result in directory

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -13,3 +13,15 @@ echo $R2
 
 R3=$(($c+$a/$b))
 echo $R3
+
+R4=$(($a%$b+$c))
+echo $R4
+
+declare -A expressions
+expressions[R1]=$R1
+expressions[R2]=$R2
+expressions[R3]=$R3
+expressions[R4]=$R4
+echo ${!expressions[@]}
+echo ${expressions[@]}
+
